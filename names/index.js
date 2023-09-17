94e8d1c56ab2eb75ce0f87b9f8686aef11692d30
root <shrivardhanmallekedi@gmail.com>
2023-09-17 10:30:07 +0000

    First Names of the People solution

---
+++ names/index.js
@@ -0,0 +1,7 @@
+let peopleNames = require("../country/state/city/index.js");
+let getFirstNames = require("../utilities/utils/index.js");
+const getPeopleInCity = (peopleNames) => {
+  return getFirstNames(peopleNames);
+};
+module.exports = getPeopleInCity;
+console.log(getPeopleInCity);
